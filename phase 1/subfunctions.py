1b3f6a2c63a978dc4d673c5503b64f2657d6db03
andreslazalde <andres_lazalde@tamu.edu>
2026-09-17 11:49:40 -0500

    fixing merge issue frfr

--- phase 1/subfunctions.py
+++ phase 1/subfunctions.py
@@ -15,7 +15,7 @@ def get_mass(rover):
         m_total = m_wheels + m_spd_rdcr + m_motor+ m_chassis + m_payload + m_pwr_systm
         return m_total
 
-'''def get_gear_ratio(speed_reducer):
+def get_gear_ratio(speed_reducer):
     if type(speed_reducer) != "dict":
         raise TypeError("Input is not a dict, fuck you")
     else:
