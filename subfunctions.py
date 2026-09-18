bc772c590899eda494a46ff457c35ff46f6ed2ef
Jacob Cotter <jmcotter@tamu.edu>
2026-09-18 13:42:44 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -133,11 +133,11 @@ def F_rolling(omega, terrain_angle, rover, planet, crr):
         v = omega * get_gear_ratio(rover["wheel_assembly"]["speed_reducer"]) * rover["wheel_assembly"]["wheel"]["radius"]
         frr = np.array([])
         for i in range(len(terrain_angle)):
-            Frr = np.append(frr, -erf(40 * v) * crr * get_mass(rover) * planet["g"] * np.cos(np.radians(terrain_angle[i])))
+            frr = np.append(frr, -erf(40 * v) * crr * get_mass(rover) * planet["g"] * np.cos(np.radians(terrain_angle[i])))
     else:
-        Frr = np.array([])
+        frr = np.array([])
         for i in range(len(omega)):
             v = omega[i] * get_gear_ratio(rover["wheel_assembly"]["speed_reducer"]) * rover["wheel_assembly"]["wheel"]["radius"]
-            Frr = np.append(Frr, -erf(40 * v) * crr * get_mass(rover) * planet["g"] * np.cos(np.radians(terrain_angle[i])))
+            frr = np.append(frr, -erf(40 * v) * crr * get_mass(rover) * planet["g"] * np.cos(np.radians(terrain_angle[i])))
         
-    return Frr
+    return frr
