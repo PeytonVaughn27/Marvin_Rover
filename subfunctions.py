7fcf5627c8fcfaa0d49bcad1fb2694113842d8d6
Jacob Cotter <jmcotter@tamu.edu>
2026-09-18 13:57:13 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -1,4 +1,3 @@
-
 from math import erf
 
 import numpy as np
@@ -82,7 +81,8 @@ def F_drive(omega, rover):
     return Fd
 
 def F_gravity(terrain_angle, rover, planet):
-    
+
+
     if type(terrain_angle) != np.ndarray:
         raise TypeError("Input is not a np.ndarray, fuck you")
     
