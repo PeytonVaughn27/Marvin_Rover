cacbd7766b849097a468ba450524e226f0f86733
Jacob Cotter <jmcotter@tamu.edu>
2026-09-18 12:49:03 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -124,17 +124,15 @@ def F_rolling(omega, terrain_angle, rover, planet, crr):
     if crr < 0:
         raise ValueError("Crr is negative, fuck you")
 
-    if type(omega) == float or type(omega) == int:
-        v = omega * rover["wheel_assembly"]["wheel"]["radius"]
-    else:
-        v = omega[0] * rover["wheel_assembly"]["wheel"]["radius"]
-
     Frr = np.array([])
     for i in range(len(terrain_angle)):
         angle = terrain_angle[i]
         if angle < -75  or angle > 75:
             raise ValueError(f"Terrain angle {angle} out of array {terrain_angle} is out of bounds, fuck you")
         
-        Frr = np.append(Frr, -erf(40 * v.astype(float)) * crr * get_mass(rover) * planet["g"] * np.cos(np.radians(angle)) / 6)
+        if type(omega) == float or type(omega) == int:
+            v = omega * 
+        
+        Frr = np.append(Frr, -erf(40 * omega[i] * rover["wheel_assembly"]["wheel"]["radius"]) * crr * get_mass(rover) * planet["g"] * np.cos(np.radians(angle)) / 6)
         
     return Frr
