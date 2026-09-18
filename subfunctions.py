b3640409e4a95742339620e53c4f6949c4d478f5
Jacob Cotter <jmcotter@tamu.edu>
2026-09-18 04:05:13 -0500

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -62,7 +62,7 @@ def F_drive(omega, rover):
     if type(rover) != dict:
         raise TypeError("Input is not a dict, fuck you")
 
-    if (type(omega) != float and type(omega) != int) and omega.ndim > 1:
+    if (type(omega) != float and type(omega) != int) and (type(omega) != np.ndarray):
         raise TypeError("Input is not a 1D vector or scalar, fuck you")
 
     tau = tau_dcmotor(omega, rover["wheel_assembly"]["motor"])
